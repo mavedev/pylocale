122eef21a2da505017e9d3be826f8a23d4dcdaab
mavedev <usatchoff@gmail.com>
2020-05-16 21:49:26 +0300

    Parsing error provided

--- errors.py
+++ errors.py
@@ -1,3 +1,8 @@
 class NoSuchLocaleError(Exception):
     """Raised when a translation file is not found."""
     pass
+
+
+class ParserInvalidLineError(Exception):
+    """Raised when some line in a locale file is invalid."""
+    pass
--- parser.py
+++ parser.py
@@ -3,12 +3,19 @@ from os.path import join
 from os import linesep
 
 import aliases
+from errors import ParserInvalidLineError
 
 
 def parse(at: aliases.Path, locale: aliases.Locale) -> aliases.Vocabulary:
     with open(join(at, locale)) as file:
         lines = file.read().split(linesep)
         lines = _get_processed_lines(lines)
+
+        if not _are_lines_valid(lines):
+            raise ParserInvalidLineError(
+                'A locale file is written with mistakes'
+            )
+
         vocabulary: aliases.Vocabulary = {}
 
 
