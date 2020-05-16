0f79ba0d92b4a6aaca77dab20678f9eb87ced850
mavedev <usatchoff@gmail.com>
2020-05-16 19:43:04 +0300

    Mode check added

--- app/parser.py
+++ app/parser.py
@@ -3,7 +3,6 @@ from os.path import join
 import app.types as types
 
 
-class Parser:
-    def parse(self, at: types.Path, locale: types.Locale) -> None:
-        with open(join(at, locale)) as file:
-            print(file.read())
+def parse(at: types.Path, locale: types.Locale) -> None:
+    with open(join(at, locale)) as file:
+        print(file.read())
--- app/pylocale.py
+++ app/pylocale.py
@@ -1,19 +1,24 @@
 import app.types as types
-from .parser import Parser
+from .parser import parse
 
 
 class PyLocale:
     def __init__(
         self,
         locales_path: types.Path,
-        root_locale: types.Locale
+        root_locale: types.Locale,
+        strict_mode=False
     ) -> None:
+        self._strict_mode = strict_mode
         self._vocabulary: types.Vocabulary = {}
         self._load_locales(locales_path, root_locale)
 
     def _load_locales(
         self, locales_path: types.Path,
-        root_locale: types.Locale
+        locale: types.Locale
     ) -> None:
-        parser = Parser()
-        parser.parse(locales_path, root_locale)
+        try:
+            parse(locales_path, locale)
+        except FileNotFoundError:
+            if self._strict_mode:
+                raise FileNotFoundError()
