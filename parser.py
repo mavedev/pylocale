d672baef7e91499836d61a305a1f7ace5a5e28ef
mavedev <usatchoff@gmail.com>
2020-05-16 21:27:20 +0300

    Lines processing implemented

--- parser.py
+++ parser.py
@@ -1,8 +1,17 @@
+from typing import List
 from os.path import join
+from os import linesep
 
 import aliases as aliases
 
 
 def parse(at: aliases.Path, locale: aliases.Locale) -> None:
     with open(join(at, locale)) as file:
-        print(file.read())
+        lines = file.read().split(linesep)
+        lines = _get_processed_lines(lines)
+
+
+def _get_processed_lines(lines: List[str]) -> List[str]:
+    processed: List[str] = []
+    for line in lines:  # type: str
+        processed.append(' '.join(line.split()))
